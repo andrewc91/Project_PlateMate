908a3d4fb31c3a7e0268c06230c55a2855dc6901
Andrew Choe <achoe91@uw.edu>
2017-01-31 22:16:18 -0800

    fixing

--- movie_review/apps/movie_app/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-from . import views
-
-urlpatterns = [
-    url(r'^$', views.index, name="index"),
-    url(r'^new$', views.new, name="new"),
-    url(r'^watch$', views.watch, name="watch"),
-    url(r'^create$', views.create, name="create")
-]
